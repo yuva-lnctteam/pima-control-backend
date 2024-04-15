a5fb5b59c189277bcb176f7d5d0e9145f49d9c3c
Pulkit Gupta <pulkitgupta95700@gmail.com>
2024-04-15 11:51:47 +0530

    Update package-lock.json and mongodb config.js

--- src/databases/mongodb/config.js
+++ src/databases/mongodb/config.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 const DB_NAME = process.env.DB_NAME;
 const connectToMongoDB = () => {
-  mongoose
-    .connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.log("Failed to connect to MongoDB", err);
-    });
+    mongoose
+        .connect(`${process.env.MONGODB_ATLAS_URI}/${DB_NAME}`)
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+        .catch((err) => {
+            console.log("Failed to connect to MongoDB", err);
+        });
 };
 
 module.exports = connectToMongoDB;
