b466ca4c0683176b60f4ccdd5adbccb09ddd6f68
Harsh Gujrati <harshgujrati2003@gmail.com>
2024-04-18 15:52:54 +0530

    uncomment public

--- src/api/routes/public.js
+++ src/api/routes/public.js
@@ -1,32 +1,31 @@
-//THIS FILE IS USED FOR CERTIFICATE GENERATION
 
-// const express = require("express");
-// const router = express.Router();
-// const basicAuth = require('express-basic-auth');
+const express = require("express");
+const router = express.Router();
+const basicAuth = require('express-basic-auth');
 
-// // Basic Authentication middleware
-// const userId = process.env.userId;
-// const userPassword = process.env.userPassword;
+// Basic Authentication middleware
+const userId = process.env.userId;
+const userPassword = process.env.userPassword;
 
-// const userAuth = basicAuth({
-//     users: { [userId]: userPassword }, 
-//     challenge: true, 
-//     unauthorizedResponse: 'Unauthorized',
-//   });
+const userAuth = basicAuth({
+    users: { [userId]: userPassword }, 
+    challenge: true, 
+    unauthorizedResponse: 'Unauthorized',
+  });
 
-// // My models
-// const User = require("../../databases/mongodb/models/User");
-// const Course = require("../../databases/mongodb/models/Course");
+// My models
+const User = require("../../databases/mongodb/models/User");
+const Course = require("../../databases/mongodb/models/Course");
 
-// const statusText = require("../../utilities/status_text.js");
-// const { vars } = require("../../utilities/constants");
-// const {
-//   isoDateStringToDDMMYYY,
-//   isRequiredUnitActivityPresent,
-// } = require("../../utilities/helper_functions");
-// const Vertical = require("../../databases/mongodb/models/Vertical");
+const statusText = require("../../utilities/status_text.js");
+const { vars } = require("../../utilities/constants");
+const {
+  isoDateStringToDDMMYYY,
+  isRequiredUnitActivityPresent,
+} = require("../../utilities/helper_functions");
+const Vertical = require("../../databases/mongodb/models/Vertical");
 
-// // ! what if the user's activity field is not present, and we include it in the projection
-// // todo: verify vertical id and correct unit srch for loop
+// ! what if the user's activity field is not present, and we include it in the projection
+// todo: verify vertical id and correct unit srch for loop
 
-// module.exports = router;
+module.exports = router;
